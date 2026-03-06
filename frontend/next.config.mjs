acf059978e309fea9dba4323b5d17b3f46d2cb1d
itsarijeetaws <arijitde89@gmail.com>
2026-03-07 01:46:56 +0530

    fix: update dependencies and tsconfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,14 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(process.cwd())
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
